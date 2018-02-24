f243034203fe08e69642ccc00deb16a06df74fb3
Teemu Rytilahti <tpr@iki.fi>
2018-02-24 19:14:37 +0100

    SongpalException: add code and message variables, to be used for storing those coming from the device

--- songpal/common.py
+++ songpal/common.py
@@ -1,2 +1,12 @@
 class SongpalException(Exception):
-    pass
+    """This exception class is used to wrap exceptions coming from this lib.
+
+    In case of an error from the endpoint device, the delivered error code
+    and the corresponding message is stored in `code` and `message` variables
+    accordingly.
+    """
+    def __init__(self, *args, error=None):
+        super().__init__(*args)
+        self.code = self.message = None
+        if error is not None:
+            self.code, self.message = error
