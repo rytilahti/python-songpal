c897a5b353f3ce33c826bc8a677324ede219ae79
jwiese <jwiese@users.noreply.github.com>
2022-07-09 14:47:52 -0600

    Update play_info to behave properly for zones

--- songpal/device.py
+++ songpal/device.py
@@ -178,7 +178,8 @@ class Device:
     async def get_play_info(self) -> PlayInfo:
         """Return  of the device."""
         info = await self.services["avContent"]["getPlayingContentInfo"]({})
-        return PlayInfo.make(**info.pop())
+        return[PlayInfo.make(services=self.services, **x)
+            for x in info]
 
     async def get_power_settings(self) -> List[Setting]:
         """Get power settings."""
