09400a8b80c88af53c4c3160dd1aab69900fa219
Teemu Rytilahti <tpr@iki.fi>
2023-11-04 01:38:57 +0100

    Drop importlib_metadata dependency
    importlib.metadata is part of the stdlib since python3.8, so no more need for that.

--- songpal/__init__.py
+++ songpal/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-from importlib_metadata import version  # type: ignore
+from importlib.metadata import version
 
 from songpal.common import SongpalException
 from songpal.device import Device
